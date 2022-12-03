fc7a63d114e70d664036d6a766e2cbfe50b8a589
Jose Plasencia <josejpd3@gmail.com>
2022-12-03 00:06:42 -0500

    store in database in /client/src/js/database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,6 +16,7 @@ const initdb = async () =>
 export const putDb = async (content) => {
     const jateDb = await openDB('jate', 1);
     const tx = jateDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
 };
 
 // TODO: Add logic for a method that gets all the content from the database
