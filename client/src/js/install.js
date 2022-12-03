10e026c7641af71f0a147fae2b22193f8b29aea8
Jose Plasencia <josejpd3@gmail.com>
2022-12-03 03:38:05 -0500

    hide install button (button click listener) in client/src/js/install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,9 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.style.display = 'none';
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
