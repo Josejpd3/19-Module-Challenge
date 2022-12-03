24f1005d5c4734c7d09b1f5cf2b530c139ce1845
Jose Plasencia <josejpd3@gmail.com>
2022-12-03 03:35:46 -0500

    stash deferredPrompt event (beforeinstallprompt) in client/src/js/install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,6 +4,7 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     e.preventDefault();
+    window.deferredPrompt = e;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
