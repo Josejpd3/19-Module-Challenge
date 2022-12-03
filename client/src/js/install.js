da9a5534a726c5d6f33e4a77513af5ff5fe41294
Jose Plasencia <josejpd3@gmail.com>
2022-12-03 03:44:40 -0500

    changed function (event) -> (e) in client/src/js/install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,7 +2,7 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {
+window.addEventListener('beforeinstallprompt', (e) => {
     e.preventDefault();
     window.deferredPrompt = e;
     butInstall.style.display = 'block';
